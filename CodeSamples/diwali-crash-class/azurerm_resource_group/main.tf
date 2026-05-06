b825eb2c3beb72bd53ec77823e605f4160c35858
erkumarsunil5786 <erkumarsunil5786@users.noreply.github.com>
2026-05-06 18:38:51 +0000

    Auto-sync repo

--- CodeSamples/diwali-crash-class/azurerm_resource_group/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "rg_group" {
-  for_each   = var.rgs
-  location   = each.value.rg_location
-  name       = each.value.rg_name
-  managed_by = each.value.managed_by
-  tags       = each.value.tags
-}
